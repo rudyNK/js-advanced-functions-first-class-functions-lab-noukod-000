3137feafeb4a76e232254a7a4c5cb90386b11627
Rudy Ravelin <rudy@noukod.com>
2019-03-05 16:49:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-onst returnFirstTwoDrivers = function (drivers) {
+
+// Declare a variable with `const` that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the **first** two drivers in the array.
+const returnFirstTwoDrivers = function(drivers) {
   return drivers.slice(0, 2);
 };
 
- const returnLastTwoDrivers = function (drivers) {
+// Declare a variable with `const` that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the **last** two drivers in the array.
+const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(-2);
-};
+}
 
- const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// This is an array containing two elements: the two functions that we previously defined (`returnFirstTwoDrivers()` and `returnLastTwoDrivers()`).
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
- const createFareMultiplier = function (multiplyValue) {
-  return function(value) {
-    return multiplyValue * value;
+// This is a higher-order function that takes in one argument, an integer, and returns a function that will multiply a fare for a ride accordingly. If `createFareMultiplier()` receives an argument of `4`, it will return a function that takes in a fare as an argument and quadruples the fare.
+function createFareMultiplier(int) {
+  return function(fare) {
+    return fare * int;
   };
-};
+}
 
- const fareDoubler = createFareMultiplier(2);
+// Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()` function accepts a fare as its lone argument and doubles it.
+const fareDoubler = createFareMultiplier(2);
 
- const fareTripler = createFareMultiplier(3);
+// Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareTripler()` function accepts a fare as its lone argument and triples it.
+const fareTripler = createFareMultiplier(3);
 
- // const fetchSpecifiedDrivers = function (drivers)
-function fetchSpecifiedDrivers(drivers, whichDrivers) {
-  return whichDrivers(drivers);
+// This function takes two arguments, an array of `drivers` and either the `returnFirstTwoDrivers()` or `returnLastTwoDrivers()` function. Based on these two arguments, `fetchSpecifiedDrivers()` will return either the first two drivers or the last two drivers.
+function fetchSpecifiedDrivers(drivers, specified) {
+  return specified(drivers);
 }
